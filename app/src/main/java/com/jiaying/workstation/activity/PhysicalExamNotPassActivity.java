abfde4487e6817a9a9c6ff6cdcd90f06177b6af8
libo <353510746@qq.com>
2016-02-25 23:59:17 +0800

    调整查询

---
+++ app/src/main/java/com/jiaying/workstation/activity/PhysicalExamNotPassActivity.java
@@ -0,0 +1,34 @@
+package com.jiaying.workstation.activity;
+
+import android.os.Bundle;
+
+import com.jiaying.workstation.R;
+import com.jiaying.workstation.utils.SetTopView;
+
+/**
+ * 体检没有通过的
+ */
+public class PhysicalExamNotPassActivity extends BaseActivity {
+    @Override
+    public void initVariables() {
+
+    }
+
+    @Override
+    public void initView() {
+        setContentView(R.layout.activity_physical_exam_not_pass);
+        new SetTopView(this,R.string.title_activity_physical_not_pass,true);
+    }
+
+    @Override
+    public void loadData() {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
--- app/src/main/java/com/jiaying/workstation/fragment/DispatchFragment.java
+++ app/src/main/java/com/jiaying/workstation/fragment/DispatchFragment.java
@@ -12,6 +12,8 @@ import android.widget.Button;
 
 import com.jiaying.workstation.R;
 import com.jiaying.workstation.activity.IdentityCardActivity;
+import com.jiaying.workstation.activity.PhysicalExamNotPassActivity;
+import com.jiaying.workstation.activity.PhysicalExamPassActivity;
 import com.jiaying.workstation.activity.PhysicalExamTodoActivity;
 import com.jiaying.workstation.activity.PhysicalExamingActivity;
 import com.jiaying.workstation.activity.RegisterOverActivity;
@@ -23,9 +25,11 @@ import com.jiaying.workstation.constant.TypeConstant;
  */
 public class DispatchFragment extends Fragment {
 
-    private View register_over_btn;
-    private View physical_examing_btn;
-    private View physical_exam_todo_btn;
+    private Button register_over_btn;
+    private Button  physical_examing_btn;
+    private Button physical_exam_todo_btn;
+    private Button physical_exam_pass_btn;
+    private Button physical_exam_not_pass_btn;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,12 +39,16 @@ public class DispatchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dispatch, container, false);
-        register_over_btn = view.findViewById(R.id.register_over_btn);
+        register_over_btn = (Button)view.findViewById(R.id.register_over_btn);
         register_over_btn.setOnClickListener(new ClickListener());
-        physical_examing_btn = view.findViewById(R.id.physical_examing_btn);
+        physical_examing_btn =(Button) view.findViewById(R.id.physical_examing_btn);
         physical_examing_btn.setOnClickListener(new ClickListener());
-        physical_exam_todo_btn = view.findViewById(R.id.physical_exam_todo_btn);
+        physical_exam_todo_btn =(Button) view.findViewById(R.id.physical_exam_todo_btn);
         physical_exam_todo_btn.setOnClickListener(new ClickListener());
+        physical_exam_pass_btn = (Button) view.findViewById(R.id.physical_exam_pass_btn);
+        physical_exam_pass_btn.setOnClickListener(new ClickListener());
+        physical_exam_not_pass_btn = (Button) view.findViewById(R.id.physical_exam_not_pass_btn);
+        physical_exam_not_pass_btn.setOnClickListener(new ClickListener());
         return view;
     }
 
@@ -48,7 +56,6 @@ public class DispatchFragment extends Fragment {
     private class ClickListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            Log.e("TAG","click");
             Intent it = null;
             switch (v.getId()) {
                 case R.id.register_over_btn:
@@ -63,7 +70,14 @@ public class DispatchFragment extends Fragment {
                     //体检中
                     it = new Intent(getActivity(), PhysicalExamingActivity.class);
                     break;
-
+                case R.id.physical_exam_pass_btn:
+                    //体检通过的
+                    it = new Intent(getActivity(), PhysicalExamPassActivity.class);
+                    break;
+                case R.id.physical_exam_not_pass_btn:
+                    //体检没有通过的
+                    it = new Intent(getActivity(), PhysicalExamNotPassActivity.class);
+                    break;
                 default:
                     break;
             }
